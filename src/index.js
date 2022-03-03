e13cfe3290ec1f2d483fca6aa80e2fcf3dd44fa0
Erin Van Brunt <erinmontybruce@gmail.com>
2022-03-03 15:44:49 -0800

    saves & displays new toys created. form resets. Need to make like button work

--- src/index.js
+++ src/index.js
@@ -52,31 +52,19 @@ document.addEventListener("DOMContentLoaded", () => {
       form.addEventListener("submit", addNewToy);
       function addNewToy(e) {
         e.preventDefault();
-        console.log("😏 Added: ", e.target.name.value);
+
+        //console.log("😏 Added: ", e.target.name.value);
         const toy = {
           name: e.target.name.value,
           image: e.target.image.value,
           likes: 0,
         };
-        toyCollectionContainer.append(toy);
+        displayToyCardsIndividually(toy);
+        form.reset();
       }
     } else {
       //if button not clicked, don't display toy container
       toyFormContainer.style.display = "none";
     }
   });
-
-  // function displayRamenObj(ramen) {
-  //   const ramenImg = document.createElement("img"); // creates new img el called ramenImg
-  //   ramenImg.src = ramen.image; // gives that el a src image from the json
-  //   menu.append(ramenImg); //adds this image to the menu (put in specific place)
-  //   //the 1st class fxn above (addRamenToPage) will run this fxn for every item in array
-
-  //   //fxn created to deal with click image to change things in ramenDetail div
-  //   ramenImg.addEventListener("click", () => {
-  //     //console.log("🪐", ramen);
-  //     displayRamenDetails(ramen); //this function is declared below and takes in the
-  //     //specifically clicked el in ramenArr
-  //   });
-  // }
 });
