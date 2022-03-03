7ef57cb07a4deb5d3293b1c519c93c9d3e50c22c
Erin Van Brunt <erinmontybruce@gmail.com>
2022-03-03 15:29:02 -0800

    displaying pre made toys, and storing new Toy. new today not displayed in card

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 let addToy = false;
+
 document.addEventListener("DOMContentLoaded", () => {
   //-----------------FETCH TOY JSON
   const toyAPI = "http://localhost:3000/toys";
@@ -9,18 +10,61 @@ document.addEventListener("DOMContentLoaded", () => {
 
   //-----------------GLOBAL VARS
   const addBtn = document.querySelector("#new-toy-btn");
+  const toyCollectionContainer = document.querySelector("#toy-collection");
   const toyFormContainer = document.querySelector(".container");
   const form = document.querySelector("form");
 
   //-----------------RENDER API TOYS() renders the toys already in the API
   function renderPreMadeToys(toysArr) {
-    console.log("🧸", toysArr);
+    //console.log("🧸", toysArr);
     toysArr.forEach(displayToyCardsIndividually);
   }
 
   function displayToyCardsIndividually(toy) {
-    console.log(toyFormContainer);
+    let h2 = document.createElement("h2");
+    h2.innerText = toy.name;
+
+    let img = document.createElement("img");
+    img.src = toy.image;
+    img.alt = toy.image;
+    img.setAttribute("class", "toy-avatar");
+
+    let p = document.createElement("p");
+    p.innerText = `likes: ${toy.likes}`;
+
+    let button = document.createElement("button");
+    button.innerText = "like";
+    button.setAttribute("class", "like-btn");
+    button.setAttribute("id", toy.id);
+
+    let toyCardDiv = document.createElement("div");
+    toyCardDiv.append(h2, img, p, button);
+    toyCardDiv.setAttribute("class", "card");
+
+    toyCollectionContainer.append(toyCardDiv);
   }
+
+  //-----------------EVENT LISTER() for add new toy button
+  addBtn.addEventListener("click", () => {
+    addToy = !addToy;
+    if (addToy) {
+      toyFormContainer.style.display = "block";
+      form.addEventListener("submit", addNewToy);
+      function addNewToy(e) {
+        e.preventDefault();
+        console.log("😏 Added: ", e.target.name.value);
+        const toy = {
+          name: e.target.name.value,
+          image: e.target.image.value,
+          likes: 0,
+        };
+        toyCollectionContainer.append(toy);
+      }
+    } else {
+      //if button not clicked, don't display toy container
+      toyFormContainer.style.display = "none";
+    }
+  });
 
   // function displayRamenObj(ramen) {
   //   const ramenImg = document.createElement("img"); // creates new img el called ramenImg
@@ -35,26 +79,4 @@ document.addEventListener("DOMContentLoaded", () => {
   //     //specifically clicked el in ramenArr
   //   });
   // }
-
-  //-----------------EVENT LISTER() for add new toy button
-  addBtn.addEventListener("click", () => {
-    addToy = !addToy;
-    if (addToy) {
-      toyFormContainer.style.display = "block";
-      form.addEventListener("submit", addNewToy);
-      function addNewToy(e) {
-        e.preventDefault();
-        //console.log("😏 Added: ", e.target.name.value);
-        const newToy = {
-          name: e.target.name.value,
-          image: e.target.image.value,
-          likes: 0,
-        };
-        toyFormContainer.append(newToy);
-      }
-    } else {
-      //if button not clicked, don't display toy container
-      toyFormContainer.style.display = "none";
-    }
-  });
 });
